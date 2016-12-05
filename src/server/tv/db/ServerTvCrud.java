7903257ed81029b38cd8a45e63a8faa7534adaef
ivan16luis <ivanlugiz16@yahoo.com>
2016-12-05 18:10:06 -0500

    conexion data java part 2

--- src/server/tv/db/ServerTvCrud.java
+++ src/server/tv/db/ServerTvCrud.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package server.tv.db;
 
 /**
@@ -11,5 +10,10 @@ package server.tv.db;
  * @author Luis
  */
 public class ServerTvCrud {
-    
+    /*
+    select emp.EMPLEADO_NOMBRE, emp.EMPLEADO_APELLIDO 
+    from mydbTelevision.USUARIO as us, mydbTelevision.EMPLEADO as emp 
+    where emp.USUARIO_CODIGO = us.USUARIO_CODIGO and 
+    us.USUARIO_NOMBRE = 'test_emp' and us.USUARIO_CLAVE = '456.789';
+     */
 }
